78701d4772d0ae48999228b3afa76287f62f84fc
Glitch (gmolinamatus-info301-3-3) <none>
2021-02-22 20:48:51 +0000

    🌓🎅 Checkpoint ./public/index.html:27683896/100 ./public/script.js:27683896/421

--- public/script.js
+++ public/script.js
@@ -5,4 +5,11 @@
 */
 //note the square brackets to denote an array and each value enclosed in quotes
 
+//creating the array with 
+var places = ["Salisbury", "Fruitland", "Clinton", "Veracruz", "Chicapa"];
 
+function processPlaces(){ //creating the fuction
+  for(var i = 0; i < 5; i++){ //making a for loop that will stop at 4 since i have 5 inputs on the array. Don't forget that the arrays starts a 0
+    
+  }
+}
