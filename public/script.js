90c43029eac0bac175b5a303274cffebe979cfc5
Glitch (gmolinamatus-info301-3-3) <none>
2021-02-22 21:11:59 +0000

    🛩🐞 Checkpoint ./public/script.js:27683896/181

--- public/script.js
+++ public/script.js
@@ -6,13 +6,14 @@
 //note the square brackets to denote an array and each value enclosed in quotes
 
 //creating the array with 
-var cities = ["Salisbury, MD", "Fruitland, MD", "Clinton, MD", "Veracruz, Ver, Mexico", "Chicapa, Oax, Mexico"];
-var states = ["MD", "", "", "", ""]
+var cities = ["Salisbury", "Fruitland", "Clinton", "Veracruz", "Chicapa"];
+var states = ["MD", "MD", "MD", "Ver", "Oax"];
+var country = ["USA","USA","USA","MEX","MEX"];
 
 function processPlaces(){ //creating the fuction
   for(var i = 0; i < 5; i++){ //making a for loop that will stop at 4 since i have 5 inputs on the array. Don't forget that the arrays starts a 0
     var listItem = "item" + (i+1);
-    document.getElementById(listItem).innerHTML = places[i];
+    document.getElementById(listItem).innerHTML = cities[i] + ", " + states[i] + ", " + country[i];
   }
 }
 
